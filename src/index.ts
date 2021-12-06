42642899f374c85113ca340bcaba4c752b84f222
sgash708 <sugash7081023@gmail.com>
2021-12-06 13:40:51 +0900

    標準入力変更

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { Check } from './check';
 import { readdirSync, statSync } from 'fs';
 
 const check: Check = new Check();
+let fileName: string;
+
+// REF: https://maku77.github.io/nodejs/io/readline-from-keyboard.html
 const rl: readline.Interface = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -13,11 +16,17 @@ rl.question('権限を検索したいパスを入力してください。e.g.) ~
   const perfectPath: string = answer.replace(/~\//g, `${homedir()}/`);
 
   if (statSync(perfectPath).isFile()) {
-    console.log(perfectPath, check.checkPermission(perfectPath));
+    // REF: https://tektektech.com/javascript-get-fileinfo-from-path/
+    const res: RegExpMatchArray | null = perfectPath.match(".+/(.+?)([\?#;])?$");
+    fileName = res ? res[1] : perfectPath;
+    console.log(fileName, check.checkPermission(perfectPath));
   } else {
     const files: string[] = readdirSync(perfectPath);
     files.forEach(fileName => {
-      console.log(fileName, check.checkPermission(perfectPath + fileName));
+      console.log(fileName, check.checkPermission(`${perfectPath}/${fileName}`));
     });
   }
+
+  // 入力を終了する
+  rl.close();
 });
