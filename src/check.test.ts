960e754e089e5d789f7d4a59ee7c405da2ca0eca
sgash708 <sugash7081023@gmail.com>
2021-12-08 22:18:41 +0900

    テストケース修正

--- src/check.test.ts
+++ src/check.test.ts
@@ -2,16 +2,17 @@ import { homedir } from 'os';
 import { Check } from './check';
 
 const check: Check = new Check();
+const home: string = homedir();
 
 describe('checkPermission', () => {
   it('success_exist_directory', () => {
-    expect(check.checkPermission(`${homedir}/.ssh/`)).toBe('700');
+    expect(check.checkPermission(`${home}/.ssh/`)).toBe('700');
   });
   it('exception_not_exist_dir', () => {
     expect(() => check.checkPermission(`./.ssh_test`)).toThrow("ENOENT: no such file or directory, stat './.ssh_test'");
   });
   it('success_exist_file', () => {
-    expect(check.checkPermission(`${homedir}/.bash_history`)).toBe('600');
+    expect(check.checkPermission(`${home}/.bash_history`)).toBe('600');
   });
   it('exception_not_exist_file', () => {
     // REF: https://teratail.com/questions/259723
@@ -21,7 +22,7 @@ describe('checkPermission', () => {
 
 describe('convertPath', () => {
   it('success_including_tilde', () => {
-    expect(check.convertPath('~/.ssh/authorized_keys')).toBe(`${homedir()}/.ssh/authorized_keys`);
+    expect(check.convertPath('~/.ssh/authorized_keys')).toBe(`${home}/.ssh/authorized_keys`);
   });
   it('success_including_prefix_tilde', () => {
     expect(check.convertPath('~~test.txt')).toBe('~~test.txt');
@@ -33,15 +34,15 @@ describe('convertPath', () => {
     expect(check.convertPath('~test~/nice.txt')).toBe('~test~/nice.txt');
   });
   it('success_not_including_tilde', () => {
-    expect(check.convertPath(`${homedir()}/.ssh/authorized_keys`)).toBe(`${homedir()}/.ssh/authorized_keys`);
+    expect(check.convertPath(`${home}/.ssh/authorized_keys`)).toBe(`${home}/.ssh/authorized_keys`);
   });
 });
 
 describe('pickOutFileNameFromPath', () => {
   it('success_including_file_without_extension', () => {
-    expect(check.pickOutFileNameFromPath(`${homedir}/.ssh/authorized_keys`)).toBe('authorized_keys');
+    expect(check.pickOutFileNameFromPath(`${home}/.ssh/authorized_keys`)).toBe('authorized_keys');
   });
   it('success_including_file_with_extention', () => {
-    expect(check.pickOutFileNameFromPath(`${homedir}/.ssh/iam.pem`)).toBe('iam.pem');
+    expect(check.pickOutFileNameFromPath(`${home}/.ssh/iam.pem`)).toBe('iam.pem');
   });
 });
